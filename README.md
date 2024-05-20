6e83baccca3ee8128dc3de2aaaa2e6848b51bbdc
Anuj-Khadka <anujkhadka008@gmail.com>
2024-05-20 16:37:17 -0400

    Initiate Project

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # InnoDuo_Honors
 This is the record management system for honors students at Caldwell University.
+
+
+Color Codes:
+Primary: #fff
+Secondary: #4E4E4E
+CTA/Tertiary: #000
---
+++ frontend/InnoDuo/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../assets/css/navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        Good
+    </div>
+  )
+}
+
+export default Navbar
