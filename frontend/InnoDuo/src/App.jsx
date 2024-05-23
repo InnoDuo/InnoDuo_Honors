7be38083ddf57f64a9dc47d082bd3c3c78146839
Anuj-Khadka <anujkhadka008@gmail.com>
2024-05-23 15:47:08 -0400

    Update Home Page

--- frontend/InnoDuo/src/App.jsx
+++ frontend/InnoDuo/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import HomePage from './components/HomePage'
 import Navbar from './components/Navbar'
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <Navbar />
+      <HomePage />
     </>
   )
 }
