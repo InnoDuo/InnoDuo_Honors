6a52c65f470340617d5331e2ed9b0e920f84b5c0
Anuj-Khadka <anujkhadka008@gmail.com>
2024-05-24 15:10:22 -0400

    Update Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import "../assets/css/navbar.css";
 import uniLogoTrans from "../assets/images/uni-logo-trans.png";
 
