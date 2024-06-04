47e3fd030a7989966fb1f0c01caa00ed2f49b921
Anuj-Khadka <anujkhadka008@gmail.com>
2024-06-04 00:03:20 -0400

    add students record logs

--- frontend/src/components/Students.jsx
+++ frontend/src/components/Students.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import CustomTable from "./microcomponents/CustomTable";
+import '../assets/css/students.css'
+import SearchBar from "./microcomponents/SearchBar";
 
 const Students = () => {
   return (
-    <div>Students</div>
-  )
-}
+    <div className="students-container page-container">
+      <h2>Honors Students List</h2>
+      <SearchBar />
+      <CustomTable
+        colNo={70}
+        cols={[
+          "Student Name",
+          "ID",
+          "Major",
+          "Advisor",
+          "Graduation Year",
+          "Status",
+        ]}
+      />
+    </div>
+  );
+};
 
-export default Students
+export default Students;
