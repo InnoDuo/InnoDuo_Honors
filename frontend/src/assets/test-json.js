3faf88d1534664a1ac5d1e879ca4ab30361dbca8
Anuj-Khadka <anujkhadka008@gmail.com>
2024-06-16 23:52:01 -0400

    Add test file for students catalog

---
+++ frontend/src/assets/test-json.js
@@ -0,0 +1,27 @@
+const cat1 = "Honors Seminar";
+const cat2 = "Honors Core Classes";
+const cat3 = "Freshman Seminar";
+const cat4 = "Research Methodology";
+const cat5 = "CRACAD Presentation";
+const cat6 = "Service Events";
+
+const stuCatalog = [
+  {
+    stuID: 445623,
+    "cat1": {
+        catName: 'Honors Seminar',
+      item1: {
+        itemName: "FS187-007 Freshman Seminar",
+        itemStatus: "completed",
+        itemReq: "required",
+      },
+      item2: {
+        itemName: "FS187-002 Freshman Seminar",
+        itemStatus: "not started",
+        itemReq: "not required", 
+      },
+    },
+  },
+];
+
+export default stuCatalog;
