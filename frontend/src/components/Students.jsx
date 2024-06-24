afeab981ffb81a3605661b77829bb653f87e7c89
nta45 <nathantayele@gmail.com>
2024-06-24 19:27:37 -0400

    adding authcontext

--- frontend/src/components/Students.jsx
+++ frontend/src/components/Students.jsx
@@ -4,13 +4,16 @@ import '../assets/css/students.css'
 import SearchBar from "./microcomponents/SearchBar";
 import { ThemeContext } from "../context/theme";
 import { homeThemeStyle } from "../App";
+import { authContext } from "../context/authContext";
 
 
 const Students = () => {
   const { defaultTheme } = useContext(ThemeContext);
+  const { loggedIn } = useContext(authContext);
   return (
     <div className="students-container page-container" style={defaultTheme === 'dark' ? homeThemeStyle : {}}>
       <h2>Honors Students List</h2>
+      <p>Logged in: {loggedIn}</p>
       <SearchBar />
       <CustomTable
         colNo={70}
